53fab84cd17746aef13b39b78c1635a2f3f66dc1
Samantha Mah <stmah@ucdavis.edu>
2024-04-05 18:04:58 -0700

    styling

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 (function() {
     'use strict';
 
-    const button = document.querySelector('button');
+    const button = document.querySelector('#switch');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
+    const sectionLink = document.querySelectorAll('a')
     let mode = 'dark';
 
     button.addEventListener('click', function() {
@@ -12,6 +13,7 @@
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
+            sectionLink.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -26,4 +28,19 @@
             mode = 'dark'
         }
     })
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+    }
 })()
