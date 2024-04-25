f9673a55f07bce12f9fc5100a5db3fda493b5deb
Samantha Mah <stmah@ucdavis.edu>
2024-04-24 22:52:08 -0700

    studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -4,38 +4,32 @@ async function getData() {
     const data = await daylist.json();
     // console.log(data);
     globalData = data;
-    document.querySelector('#music').innerHTML = outputHTML(data);
-    document.querySelector('#picker').innerHTML = createSelectList(data);
+    document.querySelector('#playlist').innerHTML = outputHTML(data);
 }
 
 function outputHTML (data) {
     let html = '<p>';
-    html += `<p>At ${data.point1.time} I listened to ${[data.point1.mood]} while ${data.point1.activity}</p>`;
+    html += `<p>At <span id="time">${data.point1.time}</span> I listened to <span id="daylist">${[data.point1.mood]}</span> while ${data.point1.activity}</p>`;
     html += '</p>';
     return html;
 }
 
-function createSelectList(data) {
-    let html = `<option>---</option>`;
-    const dataPoints = Object.keys(data);
-    // console.log(dataPoints);
-    dataPoints.forEach(function(eachPoint){
-       html += `<option value="${eachPoint}">${data[eachPoint].time}</option>`;
-    });
-    return html;
-}
+// document.querySelector('#fa-backward-step').addEventListener('change', function() {
+//     const newValue = this.value;
+//     forwardInterface(newValue, globalData);
+// });
 
-document.querySelector('#picker').addEventListener('change', function() {
-    const newValue = this.value;
-    updateInterface(newValue, globalData);
-});
+// document.querySelector('#fa-forward-step').addEventListener('change', function() {
+//     const newValue = this.value;
+//     backwardInterface(newValue, globalData);
+// });
 
-function updateInterface(value, jsonData) {
-    // console.log(value);
-    let html = '<p>';
-    html += `At ${jsonData[value].time} I was listening to ${[jsonData[value].mood]} while ${jsonData[value].activity}`;
-    html += '</p>';
-    document.querySelector('#result').innerHTML = html;
-}
+// function forwardInterface(value, jsonData) {
+//     // console.log(value);
+//     let html = '<p>';
+//     html += `At ${jsonData[value].time} I was listening to ${[jsonData[value].mood]} while ${jsonData[value].activity}`;
+//     html += '</p>';
+//     document.querySelector('#result').innerHTML = html;
+// }
 
 getData();
