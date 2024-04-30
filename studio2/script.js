ec874e5514a6c706540002c70fe923a4978716a3
Samantha Mah <stmah@ucdavis.edu>
2024-04-29 22:51:34 -0700

    studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -2,7 +2,7 @@ let globalData;
 async function getData() {
     const daylist = await fetch('data/daylist.json');
     const data = await daylist.json();
-    // console.log(data);
+    console.log(data);
     globalData = data;
     document.querySelector('#playlist').innerHTML = outputHTML(data);
 }
@@ -14,22 +14,24 @@ function outputHTML (data) {
     return html;
 }
 
-// document.querySelector('#fa-backward-step').addEventListener('change', function() {
-//     const newValue = this.value;
-//     forwardInterface(newValue, globalData);
-// });
+document.querySelector('#fa-forward-step').addEventListener('click', function() {
+    const newValue = this.value;
+    forwardInterface(newValue, globalData);
+})
 
-// document.querySelector('#fa-forward-step').addEventListener('change', function() {
-//     const newValue = this.value;
-//     backwardInterface(newValue, globalData);
-// });
+document.querySelector('#fa-backward-step').addEventListener('change', function() {
+    const newValue = this.value;
+    forwardInterface(newValue, globalData);
+});
 
-// function forwardInterface(value, jsonData) {
-//     // console.log(value);
-//     let html = '<p>';
-//     html += `At ${jsonData[value].time} I was listening to ${[jsonData[value].mood]} while ${jsonData[value].activity}`;
-//     html += '</p>';
-//     document.querySelector('#result').innerHTML = html;
-// }
+function forwardInterface(value, jsonData) {
+    // console.log(value);
+    const image = document.querySelector('#daylist');
+    let html = '<p>';
+    html += `At ${jsonData[value].time} I was listening to ${[jsonData[value].mood]} while ${jsonData[value].activity}`;
+    html += '</p>';
+    document.querySelector('#result').innerHTML = html;
+    daylist.innerHTML = `<img id="daylist" src="images/1.jpg" alt="daylist 1">`;
+}
 
 getData();
