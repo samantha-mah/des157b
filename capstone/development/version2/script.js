d153611e63d8dffbadc795a387713d2ac17e102e
Samantha Mah <stmah@ucdavis.edu>
2024-06-05 00:46:06 -0700

    version 2

--- capstone/development/version2/script.js
+++ capstone/development/version2/script.js
@@ -435,3 +435,112 @@ Parse.serverURL = 'https://parseapi.back4app.com/';
   // displaySubmissions();
 
 })();
+
+// this is the form that will be submitted
+const form = document.querySelector('#userdata');
+
+// event listener for submitting the form
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    handleSubmit();
+});
+
+// this takes the data from the form and sends it to Back4App
+async function handleSubmit() {
+
+    // Get the form data
+    const file1 = document.getElementById('question1').files[0];
+    const text1 = document.getElementById('question2').value;
+    const text2 = document.getElementById('question3').value;
+    const text3 = document.getElementById('question4').value;
+    const text4 = document.getElementById('question5').value;
+    const text5 = document.getElementById('question6').value;
+
+
+    // Create a new Parse Object
+    const DataObject = Parse.Object.extend("Records");
+    const dataObject = new DataObject();
+
+    // Set the text fields
+    dataObject.set("q2", text1);
+    dataObject.set("q3", text2);
+    dataObject.set("q4", text3);
+    dataObject.set("q5", text4);
+    dataObject.set("q6", text5);
+
+    // Save the first file
+    if (file1) {
+        const parseFile1 = new Parse.File(file1.name, file1);
+        await parseFile1.save();
+        dataObject.set("q1", parseFile1);
+    }
+
+    // Save the second file
+    // if (file2) {
+    //     const parseFile2 = new Parse.File(file2.name, file2);
+    //     await parseFile2.save();
+    //     dataObject.set("q5", parseFile2);
+    // }
+
+    try {
+        // Save the object to Parse
+        const savedObject = await dataObject.save();
+        // get the id of the last record saved...
+        const objectId = savedObject.id;
+        // send the browser to the data.html page with the id of the record as a parameter
+        window.location.href = './data.html?id=' + objectId;
+    } catch (error) {
+        alert("Failed to save data: " + error.message);
+    }
+}
+
+//Get the record ID from the URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const recordId = urlParams.get('id');
+//alert(recordId);
+
+//add variables for where to stick the content on the page
+const a1 = document.querySelector('#a1');
+const a2 = document.querySelector('#a2');
+const a3 = document.querySelector('#a3');
+const a4 = document.querySelector('#a4');
+const a5 = document.querySelector('#a5');
+const a6 = document.querySelector('#a6');
+
+// This function asychronously gets the data from the record and puts it on the page
+async function retrieveRecord(recordId) {
+    // Create a query for the specific class (in this case I named the table Records)
+    const DataObject = Parse.Object.extend("Records");
+    const query = new Parse.Query(DataObject);
+
+    try {
+        // Get the object by ID
+        const dataObject = await query.get(recordId);
+
+        // Retrieve the data
+        const text1 = dataObject.get("q1");
+        const text2 = dataObject.get("q2");
+        const file1 = dataObject.get("q3");
+        const text4 = dataObject.get("q4");
+        const file2 = dataObject.get("q5");
+
+
+        a1.innerHTML =`<img src="${file1.url()}" alt="image 1">`;
+        a2.innerHTML =`a2 ${text2}`;
+        a3.innerHTML =`a3 ${text3}`;
+        a4.innerHTML =`a4 ${text4}`;
+        a5.innerHTML =`a5 ${text5}`;
+        atob6.innerHTML =`a6 ${text6}`;
+
+        
+    } catch (error) {
+        console.error("Error while retrieving record: " + error.message);
+    }
+}
+
+// This if statement makes sure the retrieveRecord function ony runs if an id is there
+// Probably more should be done to handle errors here.
+if(recordId){
+    retrieveRecord(recordId);
+}
